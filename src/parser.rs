461bbdc4f18cdd3572ad575e6a40d35bd2e86a1a
Cyryl Płotnicki <cyplo@cyplo.net>
2016-09-11 11:04:08 +0200

    Racer crash test files

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,20 @@
 use std::str::FromStr;
 use chrono::*;
 
-fn parse_time(line: &str) -> DateTime<FixedOffset> {
-    DateTime::from_str(line).unwrap()
+fn parse_time(line: &str) -> Result<DateTime<FixedOffset>, ParseError> {
+    let date_format = "%d/%b/%Y";
+    let time_format = "%H:%M:%S";
+    let timezone_format = "%z";
+    let timestamp_format = date_format + ":" + time_format + " " + timezone_format;
+    let mut parsed_data = format::  Parsed::new();
+    let parse_result = parse(&parsed_data, line);
+    DateTime::from_str(line)
 }
 
 #[test]
-fn can_parse_timestamp() {
-    let line = "[10/Sep/2016:18:35:47 +0200]";
-    let timestamp = parse_time(line);
+fn timestamp_should_have_proper_time_offset() {
+    let line = "10/Sep/2016T18:35:47 +0200";
+    let timestamp = parse_time(line).unwrap();
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::east(2 * 3600))
 }
