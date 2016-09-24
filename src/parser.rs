b49a1463f5ef456d44016748d8e84235b8b06646
Cyryl Płotnicki <cyplo@cyplo.net>
2016-09-24 12:46:42 +0200

    rename the parser function

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,14 @@
 use std::str::FromStr;
 use chrono::*;
 
-fn parse_time(text: &str) -> Result<DateTime<FixedOffset>, ParseError> {
+fn parse_datetime(text: &str) -> Result<DateTime<FixedOffset>, ParseError> {
     DateTime::parse_from_str(text, "[%d/%b/%Y:%H:%M:%S %z]")
 }
 
 #[test]
 fn has_proper_positive_timezone() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::east(2 * 3600))
 }
@@ -16,14 +16,14 @@ fn has_proper_positive_timezone() {
 #[test]
 fn has_proper_negative_timezone() {
     let line = "[10/Sep/2016:18:35:47 -0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::west(2 * 3600))
 }
 #[test]
 fn has_proper_date_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let date = timestamp.date();
     assert_eq!(date, UTC.ymd(2016,09,10))
 }
@@ -31,7 +31,7 @@ fn has_proper_date_when_midnight() {
 #[test]
 fn has_proper_date() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let date = timestamp.date();
     assert_eq!(date, UTC.ymd(2016,09,10))
 }
@@ -39,7 +39,7 @@ fn has_proper_date() {
 #[test]
 fn has_proper_time() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let time = timestamp.time();
     assert_eq!(time, NaiveTime::from_hms(18,35,47))
 }
@@ -47,7 +47,7 @@ fn has_proper_time() {
 #[test]
 fn has_proper_time_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
-    let timestamp = parse_time(line).unwrap();
+    let timestamp = parse_datetime(line).unwrap();
     let time = timestamp.time();
     assert_eq!(time, NaiveTime::from_hms(0,0,0))
 }
