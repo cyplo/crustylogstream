d256587dde1f2a32bd1a5cf3fb18072f4d5eef58
Cyryl Płotnicki <cyplo@cyplo.net>
2016-10-06 14:36:57 +0200

    Reformat

--- src/parser.rs
+++ src/parser.rs
@@ -20,12 +20,13 @@ fn has_proper_negative_timezone() {
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::west(2 * 3600))
 }
+
 #[test]
 fn has_proper_date_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
     let timestamp = parse_datetime(line).unwrap();
     let date = timestamp.date();
-    assert_eq!(date, UTC.ymd(2016,09,10))
+    assert_eq!(date, UTC.ymd(2016, 09, 10))
 }
 
 #[test]
@@ -33,7 +34,7 @@ fn has_proper_date() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
     let timestamp = parse_datetime(line).unwrap();
     let date = timestamp.date();
-    assert_eq!(date, UTC.ymd(2016,09,10))
+    assert_eq!(date, UTC.ymd(2016, 09, 10))
 }
 
 #[test]
@@ -41,7 +42,7 @@ fn has_proper_time() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
     let timestamp = parse_datetime(line).unwrap();
     let time = timestamp.time();
-    assert_eq!(time, NaiveTime::from_hms(18,35,47))
+    assert_eq!(time, NaiveTime::from_hms(18, 35, 47))
 }
 
 #[test]
@@ -49,5 +50,5 @@ fn has_proper_time_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
     let timestamp = parse_datetime(line).unwrap();
     let time = timestamp.time();
-    assert_eq!(time, NaiveTime::from_hms(0,0,0))
+    assert_eq!(time, NaiveTime::from_hms(0, 0, 0))
 }
