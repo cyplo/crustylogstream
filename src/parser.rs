e4b3ef03acb7b82b4793a8af174d09c4f8509ec4
Cyryl Płotnicki <cyplo@cyplo.net>
2016-10-07 15:49:28 +0200

    Force validation of date and time format markers presence

--- src/parser.rs
+++ src/parser.rs
@@ -8,8 +8,14 @@ struct ChronoDateTimeParser {
 }
 
 impl ChronoDateTimeParser {
-    fn new() -> ChronoDateTimeParser {
-        ChronoDateTimeParser{}
+    fn new(format: &str) -> Option<ChronoDateTimeParser> {
+        if !format.contains("%t") {
+            return None;
+        }
+        if !format.contains("%d") {
+            return None;
+        }
+        Some(ChronoDateTimeParser{})
     }
 }
 
@@ -20,9 +26,27 @@ impl DateTimeParser for ChronoDateTimeParser {
 }
 
 #[test]
+fn does_not_fail_with_time_and_date_formats() {
+    let parser = ChronoDateTimeParser::new("%t:%d");
+    assert!(parser.is_some())
+}
+
+#[test]
+fn fails_without_time_format() {
+    let parser = ChronoDateTimeParser::new("%d");
+    assert!(parser.is_none())
+}
+
+#[test]
+fn fails_without_date_format() {
+    let parser = ChronoDateTimeParser::new("%t");
+    assert!(parser.is_none())
+}
+
+#[test]
 fn has_proper_positive_timezone() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::east(2 * 3600))
@@ -31,7 +55,7 @@ fn has_proper_positive_timezone() {
 #[test]
 fn has_proper_negative_timezone() {
     let line = "[10/Sep/2016:18:35:47 -0200]";
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let timezone = timestamp.timezone();
     assert_eq!(timezone, FixedOffset::west(2 * 3600))
@@ -40,7 +64,7 @@ fn has_proper_negative_timezone() {
 #[test]
 fn has_proper_date_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let date = timestamp.date();
     assert_eq!(date, UTC.ymd(2016, 09, 10))
@@ -50,7 +74,7 @@ fn has_proper_date_when_midnight() {
 fn has_proper_date() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
 
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let date = timestamp.date();
     assert_eq!(date, UTC.ymd(2016, 09, 10))
@@ -59,7 +83,7 @@ fn has_proper_date() {
 #[test]
 fn has_proper_time() {
     let line = "[10/Sep/2016:18:35:47 +0200]";
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let time = timestamp.time();
     assert_eq!(time, NaiveTime::from_hms(18, 35, 47))
@@ -68,7 +92,7 @@ fn has_proper_time() {
 #[test]
 fn has_proper_time_when_midnight() {
     let line = "[10/Sep/2016:00:00:00 +0200]";
-    let parser = ChronoDateTimeParser::new();
+    let parser = ChronoDateTimeParser::new("%d:%t").unwrap();
     let timestamp = parser.parse_datetime(line).unwrap();
     let time = timestamp.time();
     assert_eq!(time, NaiveTime::from_hms(0, 0, 0))
